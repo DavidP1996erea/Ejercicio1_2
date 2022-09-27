f28a9bf255b8625eccc36fe5f3ac9ba161105790
DavidP1996erea <david.perea@iesnervion.es>
2022-09-27 20:11:39 +0200

    e

--- src/Main_Random.java
+++ src/Main_Random.java
@@ -1,25 +1,48 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main_Random {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
+
+        /* Se crea la lista y el objeto randomAccessFile. También se inicializa el puntero que marcará en que palabra
+        estamos y se crea el fichero de salida */
         RandomAccessFile ra = null;
-        int puntero=0;
+        RandomAccessFile rw = null;
 
+        List<String> listaPalabras = new ArrayList<>();
+        int puntero = 0;
+        File ficheroSalida = new File("palabrasOrdenadas.txt");
         try {
-            ra = new RandomAccessFile(new File("palabrasSeparadas.txt"),"rw");
 
+            // Se llama al fichero correspendiente con el objeto RandomAccessFile
+            ra = new RandomAccessFile(new File("palabrasSeparadas.txt"), "rw");
+            rw = new RandomAccessFile(ficheroSalida, "rw");
 
-            while (ra.readLine()!=null) {
+            // Se recorre el fichero y se mete cada palabra en una lista
+            while (ra.readLine() != null) {
 
                 ra.seek(puntero);
-                System.out.println(ra.readLine());
 
-                puntero+=6;
+                listaPalabras.add(ra.readLine());
+
+                puntero += 6;
+            }
+
+            //Se ordena por orden alfabético
+            Collections.sort(listaPalabras);
+
+            // Se escribe en el fichero de salida
+            for (String e : listaPalabras) {
+
+               rw.writeBytes(e +"\n");
+
             }
 
 
-            
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
